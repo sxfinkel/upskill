8c27dbc2fee8170717ef225f12e7c4c8e0df4b92
Chris Mitchell <macmhicheil@icloud.com>
2017-01-21 21:50:58 +0000

    Added Stripe respose handler and error handling js

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-    root to: 'pages#home'
-    get 'about', to: 'pages#about'
-    resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new', as: 'new_contact' # the 'contact-us' is the desired path
+  root to: 'pages#home'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'about', to: 'pages#about'
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact' # the 'contact-us' is the desired path
 end
 
